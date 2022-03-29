27f388dd1cc0a490b9dc325887a29b8f3f7de276
Lisa Durdevic <93915800+LisaDurdevic@users.noreply.github.com>
2022-03-29 15:17:03 +0200

    wrapping up the input fields

--- tests/index.php
+++ tests/index.php
@@ -34,6 +34,21 @@
                     } else {
                         //$profile->getPublicProfile($_POST['userName']);
                         $profile->getPrivateAccount($_POST['userName']);
+                        $fields = array(["userName", 'Username:', $profile->userName, '"text" required autofocus'],
+                                        ['"password"', 'Password:', "", '"password" placeholder = "enter new password for reset"'],
+                                        ['"mail"', 'Mailaddress:', $profile->mail, '"email" required'],
+                                        ['"firstName"', 'First Name:', $profile->firstName, '"text"'],
+                                        ['"lastName"', 'Last Name:', $profile->lastName, '"text"'],
+                                        ['"phone"', 'Phone Number:', $profile->phone, '"text"'],
+                                        ['"urlLinkedin"', 'URL LinkedIn:', $profile->linkedin, '"text"'],
+                                        ['"urlXing"', 'URL Xing:', $profile->xing, '"text"'],
+                                        ['"urlGithub"', 'URL Github:', $profile->git, '"text"'],
+                                        ['"urlCustom"', 'URL Custom:', $profile->custom, '"text"'],
+                                        ['"fullTime"', 'Working Fulltime:', $profile->fullTime, '"checkbox"'],
+                                        ['"regionOne"', 'Preferred Working Region:', $profile->regionOne, '"text"'],
+                                        ['"regionTwo"', 'Optional Working Region:', $profile->regionTwo, '"text"'],
+                                        ['"preference"', 'Working Preference:', $profile->preference, '"text"'],
+                                        ['"leaveDate"', 'Leave Date:', $profile->leave, '"date"']);
                     }
                 //----------------------------------------------------
 
@@ -48,197 +63,25 @@
                         Data Input:
                     </h2>
                 <hr>
-                        <p class='row'>
-                            <label for='userName' class='col-sm fw-bold'>
-                                userName:
-                            </label>
-                            <input
-                                type='text'
-                                id='userName'
-                                name='userName'
-                                class='col-sm'
-                                value='<?php echo $profile->userName?>'
-                                required
-                                autofocus/>
-                        </p>
-
-                        <p class='row'>
-                            <label for='password' class='col-sm fw-bold'>
-                                Password:
-                            </label>
-                            <input
-                                type='password'
-                                id='password'
-                                name='password'
-                                class='col-sm'
-                                placeholder='enter new password to reset'
-                                />
-                        </p>
-
-                        <p class='row'>
-                            <label for='mail' class='col-sm fw-bold'>
-                                Mailaddress:
-                            </label>
-                            <input
-                                type='email'
-                                id='mail'
-                                name='mail'
-                                class='col-sm'
-                                value='<?php echo $profile->mail ?>'
-                                />
-                        </p>
-
-                        <p class='row'>
-                            <label for='firstName' class='col-sm'>
-                                First Name:
-                            </label>
-                            <input
-                                type='text'
-                                id='firstName'
-                                name='firstName'
-                                value='<?php echo $profile->firstName ?>'
-                                class='col-sm'
-                                />
-                        </p>
-
-                        <p class='row'>
-                            <label for='lastName' class='col-sm'>
-                                Last Name:
-                            </label>
-                            <input
-                                type='text'
-                                id='lastName'
-                                name='lastName'
-                                value='<?php echo $profile->lastName ?>'
-                                class='col-sm'
-                                />
-                        </p>
-
-                        <p class='row'>
-                            <label for='phone' class='col-sm'>
-                                Phone Number:
-                            </label>
-                            <input
-                                type='text'
-                                id='phone'
-                                name='phone'
-                                class='col-sm'
-                                value='<?php echo $profile->phone ?>'/>
-                        </p>
-
-                        <p class='row'>
-                            <label for='urlLinkedin' class='col-sm'>
-                                URL LinkedIn:
-                            </label>
-                            <input
-                                type='text'
-                                id='urlLinkedin'
-                                name='urlLinkedin'
-                                class='col-sm'
-                                value='<?php echo $profile->linkedin ?>'/>
-                        </p>
-                        
-                        <p class='row'>
-                            <label for='urlXing' class='col-sm'>
-                                URL Xing:
-                            </label>
-                            <input
-                                type='text'
-                                id='urlXing'
-                                name='urlXing'
-                                class='col-sm'
-                                value='<?php echo $profile->xing ?>'/>
-                        </p>
-
-                        <p class='row'>
-                            <label for='urlGithub' class='col-sm'>
-                                URL GitHub:
-                            </label>
-                            <input
-                                type='text'
-                                id='urlGithub'
-                                name='urlGithub'
-                                class='col-sm'
-                                value='<?php echo $profile->git ?>'/>
-                        </p>
-
-                        <p class='row'>
-                            <label for='urlCustom' class='col-sm'>
-                                Custom URL:
-                            </label>
-                            <input
-                                type='text'
-                                id='urlCustom'
-                                name='urlCustom'
-                                class='col-sm'
-                                value='<?php echo $profile->custom ?>'/>
-                        </p>
-
-                        <p class='row'>
-                            <label for='fullTime' class='col-sm'>
-                                Full Time:
-                            </label>
-                            <input
-                                type='checkbox'
-                                id='fullTime'
-                                name='fullTime'
-                                class='col-sm'
-                                <?php
-                                if ($profile->fullTime === 1) {
-                                ?> checked <?php
-                                } ?>
-                                />
-                        </p>
-
-                        <p class='row'>
-                            <label for='regionOne' class='col-sm'>
-                                Preferred Working Region:
-                            </label>
-                            <input
-                                type='text'
-                                id='regionOne'
-                                name='regionOne'
-                                class='col-sm'
-                                value='<?php echo $profile->regionOne ?>'/>
-                        </p>
-
-                        <p class='row'>
-                            <label for='regionTwo' class='col-sm'>
-                                Optional Working Region:
-                            </label>
-                            <input
-                                type='text'
-                                id='regionTwo'
-                                name='regionTwo'
-                                class='col-sm'
-                                value='<?php echo $profile->regionTwo ?>'/>
-                        </p>
-
-                        <p class='row'>
-                            <label for='preference' class='col-sm'>
-                                Working Preference:
-                            </label>
-                            <input
-                                type='text'
-                                id='preference'
-                                name='preference'
-                                class='col-sm'
-                                value='<?php echo $profile->preference ?>'/>
-                        </p>
-
-                        <hr>
-
-                        <p class='row'>
-                            <label for='leaveDate' class='col-sm'>
-                                Leave Date:
-                            </label>
-                            <input
-                                type='date'
-                                id='leaveDate'
-                                name='leaveDate'
-                                class='col-sm'
-                                value='<?php echo $profile->leave ?>'/>
-                        </p>
+                    <?php
+                            for ($i = 0; $i < sizeOf($fields); $i++) {
+                        ?>
+                            <p class='row'>
+                                <label for=<?php print $fields[$i][0]?> class='col-sm'>
+                                    <?php print $fields[$i][1]?>
+                                </label>
+                                <input
+                                    type= <?php print $fields[$i][3]?>
+                                    id= <?php print $fields[$i][0]?>
+                                    name= <?php print $fields[$i][0]?>
+                                    class='col-sm'
+                                    value='<?php print $fields[$i][2]?>'
+                                    <?php ($profile->fullTime === 1)? print "checked" : "" ?>
+                                    />
+                            </p>
+                        <?php
+                            }
+                        ?>
 
                         <hr>
 
