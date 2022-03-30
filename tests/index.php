5b0e9122c8989e147dceba508f8012e84b773c0a
Lisa Durdevic <93915800+LisaDurdevic@users.noreply.github.com>
2022-03-30 13:12:02 +0200

    adding more functions

--- tests/index.php
+++ tests/index.php
@@ -15,128 +15,148 @@
 
     <main class="container clearfix">
         <hr>
+        <!--input form to add new accounts, pull all accounts and to test login-->
+                
+        <a class='btn text-decoration-none text-light' href='./new.php'>
+            <button id='new' type='submit' class='btn btn-success'>
+                Add new
+            </button>
+        </a>
+        <a class='btn text-decoration-none text-light' href='./login.php'>
+            <button id='login' type='submit' class='btn btn-primary'>
+                Test login
+            </button>
+        </a>
+        <form id='all-accounts' class='btn' method='post' action='?get'>
+            <button id='all' type='submit' class='btn btn-warning'>
+                Show all public profiles
+            </button>
+        </form>
+        <form id='restart' class='btn'>
+            <button type='submit' class='btn btn-info'>
+                New Search
+            </button>
+        </form>
+        <hr>
         <!--input form for test data-->
         <?php
-            //userName necessary to see all fields
-            if (isset($_POST['userName'])) {
-                //output terminal to observe the interface's behaviour
+        //---------------THIS IS IMPORTANT!!!!----------------
+            include("../utils/Profile.inc.php");
+            $profile = new Profile;
 
-                //---------------THIS IS IMPORTANT!!!!----------------
-                include("../utils/Profile.inc.php");
-                $profile = new Profile;
+            //anytime options
+            if (isset($_GET['get'])) { //getting all accounts as array
+                include('./getAll.php');
+            
+            } else if (isset($_POST['userName'])) { //search initialized
 
-                    if (isset($_GET['save'])) { //Data Transfer by $_POST
+                    if (isset($_GET['save'])) { //sending modified data to DB
                         $profile->updateAccount($_POST);
                         $profile->updateRegistration($_POST);
-                    } else {
+
+                    } else { //pulling account data from DB
                         //$profile->getPublicProfile($_POST['userName']);
                         $profile->getPrivateAccount($_POST['userName']);
                     }
-                //----------------------------------------------------
-                $fields = array(["userName", 'Username:', $profile->userName, '"text" required autofocus'],
-                                        ['"password"', 'Password:', "", '"password" placeholder = "enter new password for reset"'],
-                                        ['"mail"', 'Mailaddress:', $profile->mail, '"email" required'],
-                                        ['"firstName"', 'First Name:', $profile->firstName, '"text"'],
-                                        ['"lastName"', 'Last Name:', $profile->lastName, '"text"'],
-                                        ['"phone"', 'Phone Number:', $profile->phone, '"text"'],
-                                        ['"urlLinkedin"', 'URL LinkedIn:', $profile->linkedin, '"text"'],
-                                        ['"urlXing"', 'URL Xing:', $profile->xing, '"text"'],
-                                        ['"urlGithub"', 'URL Github:', $profile->git, '"text"'],
-                                        ['"urlCustom"', 'URL Custom:', $profile->custom, '"text"'],
-                                        ['"fullTime"', 'Working Fulltime:', $profile->fullTime, '"checkbox"'],
-                                        ['"regionOne"', 'Preferred Working Region:', $profile->regionOne, '"text"'],
-                                        ['"regionTwo"', 'Optional Working Region:', $profile->regionTwo, '"text"'],
-                                        ['"preference"', 'Working Preference:', $profile->preference, '"text"'],
-                                        ['"leaveDate"', 'Leave Date:', $profile->leave, '"date"']);
+        //----------------------------------------------------
 
                 if (isset($profile->mail)) { //userName exists
+                    $fields = array(["userName", 'Username:', $profile->userName, '"text" required autofocus'],
+                                    ['"password"', 'Password:', "", '"password" placeholder = "enter new password for reset"'],
+                                    ['"mail"', 'Mailaddress:', $profile->mail, '"email" required'],
+                                    ['"firstName"', 'First Name:', $profile->firstName, '"text"'],
+                                    ['"lastName"', 'Last Name:', $profile->lastName, '"text"'],
+                                    ['"phone"', 'Phone Number:', $profile->phone, '"text"'],
+                                    ['"urlLinkedin"', 'URL LinkedIn:', $profile->linkedin, '"text"'],
+                                    ['"urlXing"', 'URL Xing:', $profile->xing, '"text"'],
+                                    ['"urlGithub"', 'URL Github:', $profile->git, '"text"'],
+                                    ['"urlCustom"', 'URL Custom:', $profile->custom, '"text"'],
+                                    ['"fullTime"', 'Working Fulltime:', $profile->fullTime, '"checkbox"'],
+                                    ['"regionOne"', 'Preferred Working Region:', $profile->regionOne, '"text"'],
+                                    ['"regionTwo"', 'Optional Working Region:', $profile->regionTwo, '"text"'],
+                                    ['"preference"', 'Working Preference:', $profile->preference, '"text"'],
+                                    ['"leaveDate"', 'Leave Date:', $profile->leave, '"date"']);
         ?>
-            <form id='data-input' class='container' method='post' action='?save'>
-                <hr>
-                <h2>Data Input:</h2>
-                <hr>
-            <?php
-                for ($i = 0; $i < sizeOf($fields); $i++) {
-            ?>
-                    <p class='row'>
-                        <label for=<?php print $fields[$i][0]?> class='col-sm'>
-                            <?php print $fields[$i][1]?>
-                        </label>
-                        <input
-                            type= <?php print $fields[$i][3]?>
-                            id= <?php print $fields[$i][0]?>
-                            name= <?php print $fields[$i][0]?>
-                            class='col-sm'
-                            value='<?php print $fields[$i][2]?>'
-                            <?php ($profile->fullTime === 1)? print "checked" : "" ?>
-                            />
-                    </p>
-            <?php
-                }
-            ?>
-                <hr>
-                <p class='row'>
-                    <label for='skills' class='col-sm'>
-                        Skills:
-                    </label>
-                    <select id='skills' name='skills[]' multiple='yes'>
-                        <?php
-                            for ($i = 0; $i < sizeOf($profile->skills); $i++) {
-                                print "<option selected='selected'>" . $profile->skills[$i] . "</option>";
-                            }
-                        ?>
-                    </select>
-                </p>
-
-                <p class='row'>
-                    <label for='topSkills' class='col-sm'>
-                        Top Skills:
-                    </label>
-                    <select id='topSkills' name='topSkills[]' multiple='yes'>
-                        <?php
-                            for ($i = 0; $i < sizeOf($profile->topSkills); $i++) {
-                                print "<option selected='selected'>" . $profile->topSkills[$i] . "</option>";
-                            }
-                        ?>
-                    </select>
-                </p>
-
-                <p class='row'>
-                    <label for='languages' class='col-sm'>
-                        Languages:
-                    </label>
-                    <select id='languages' name='languages[]' multiple='yes'>
-                        <?php
-                            for ($i = 0; $i < sizeOf($profile->languages); $i++) {
-                                print "<option selected = 'selected'>" . $profile->languages[$i] . "</option>"; 
-                            }
-                        ?>
-                    </select>
-                </p>
-
-                <hr>
-                <button id='send' type='submit' class='btn btn-success'>
-                    Send to DB
-                </button>
-            </form>
-
+                    <form id='data-input' class='container' method='post' action='?save'>
+                        <hr>
+                        <h2>Data Input:</h2>
+                        <hr>
         <?php
-            //Reset page for new search
-                if (isset($_POST['userName'])) {
+                        for ($i = 0; $i < sizeOf($fields); $i++) {
         ?>
-                <hr>
-                <form id='restart' class='container'>
-                    <button type='submit' class='btn btn-info'>
-                        New Search
-                    </button>
-                </form>
+                            <p class='row'>
+                                <label for=<?php print $fields[$i][0]?> class='col-sm'>
+                                    <?php print $fields[$i][1]?>
+                                </label>
+                                <input
+                                    type= <?php print $fields[$i][3]?>
+                                    id= <?php print $fields[$i][0]?>
+                                    name= <?php print $fields[$i][0]?>
+                                    class='col-sm'
+                                    value='<?php print $fields[$i][2]?>'
+                                    <?php ($profile->fullTime === 1)? print "checked" : "" ?>
+                                    />
+                            </p>
         <?php
-                    }
-                } else { //userName not found
+                        }
+        ?>
+                        <hr>
+                        <p class='row'>
+                            <label for='skills' class='col-sm'>
+                                Skills:
+                            </label>
+                            <select id='skills' name='skills[]' multiple='yes'>
+                                <?php
+                                    for ($i = 0; $i < sizeOf($profile->skills); $i++) {
+                                        print "<option selected='selected'>" . $profile->skills[$i] . "</option>";
+                                    }
+                                ?>
+                            </select>
+                        </p>
+
+                        <p class='row'>
+                            <label for='topSkills' class='col-sm'>
+                                Top Skills:
+                            </label>
+                            <select id='topSkills' name='topSkills[]' multiple='yes'>
+                                <?php
+                                    for ($i = 0; $i < sizeOf($profile->topSkills); $i++) {
+                                        print "<option selected='selected'>" . $profile->topSkills[$i] . "</option>";
+                                    }
+                                ?>
+                            </select>
+                        </p>
+
+                        <p class='row'>
+                            <label for='languages' class='col-sm'>
+                                Languages:
+                            </label>
+                            <select id='languages' name='languages[]' multiple='yes'>
+                                <?php
+                                    for ($i = 0; $i < sizeOf($profile->languages); $i++) {
+                                        print "<option selected = 'selected'>" . $profile->languages[$i] . "</option>"; 
+                                    }
+                                ?>
+                            </select>
+                        </p>
+
+                        <hr>
+                        <button id='send' type='submit' class='btn btn-success'>
+                            Send to DB
+                        </button>
+
+                        <a class='text-decoration-none text-light' href='./drop.php?userName=<?php echo $profile->userName ?>'>
+                            <button id='drop' type='button' class='btn btn-danger'>
+                                Delete Account
+                            </button>
+                        </a>
+                    </form>
+        <?php
+                } else { //userName not found, refresh site
                     header("Refresh:0");
                 }
-            } else {
-        ?>
+            } else { //no username entered, no search initialized
+        ?> 
                 <form id='userName-input' class='container' method='post'>
                     <p class='row'>
                         <label for='userName' class='col-sm fw-bold'>
